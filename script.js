633d4a0edf315cf606321699f82a8ac7ee7b06cc
IT21314742 <it21314742@my.sliit.lk>
2024-03-25 18:00:42 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function
