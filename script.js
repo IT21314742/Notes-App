611d0781f79f6b33267f460a89b0e4228832a639
IT21314742 <it21314742@my.sliit.lk>
2024-03-28 22:50:42 +0530

    Update JS
    add js

--- script.js
+++ script.js
@@ -28,7 +28,9 @@ notesContainer.addEventListener("click", function(e){
     else if(e.target.tagName == "P"){
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
-            notes.forEach(nt =>{})
+            notes.forEach(nt =>{
+                
+            })
         })
     }
 })
